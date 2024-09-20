37705e9f890e95e6d1baa751337e893207e7237a
Ali Shokoohi <shokoohi.dev@gmail.com>
2024-09-20 18:32:22 +0330

    Completed request methods

--- router/gin/gin.go
+++ router/gin/gin.go
@@ -15,12 +15,32 @@ func New() *GinRouter {
 	return &GinRouter{engine: gin.Default()}
 }
 
+func (r *GinRouter) GET(path string, handler router.HandlerFunc) {
+	r.engine.GET(path, func(c *gin.Context) {
+		handler(&ginHandler.GinContext{Ctx: c}) // Convert gin.Context to your custom Context
+	})
+}
+
 func (r *GinRouter) POST(path string, handler router.HandlerFunc) {
 	r.engine.POST(path, func(c *gin.Context) {
 		handler(&ginHandler.GinContext{Ctx: c}) // Convert gin.Context to your custom Context
 	})
 }
-
+func (r *GinRouter) PUT(path string, handler router.HandlerFunc) {
+	r.engine.PUT(path, func(c *gin.Context) {
+		handler(&ginHandler.GinContext{Ctx: c}) // Convert gin.Context to your custom Context
+	})
+}
+func (r *GinRouter) DELETE(path string, handler router.HandlerFunc) {
+	r.engine.DELETE(path, func(c *gin.Context) {
+		handler(&ginHandler.GinContext{Ctx: c}) // Convert gin.Context to your custom Context
+	})
+}
+func (r *GinRouter) OPTIONS(path string, handler router.HandlerFunc) {
+	r.engine.OPTIONS(path, func(c *gin.Context) {
+		handler(&ginHandler.GinContext{Ctx: c}) // Convert gin.Context to your custom Context
+	})
+}
 func (r *GinRouter) Serve(addr string) error {
 	return r.engine.Run(addr)
 }
--- router/router_interface.go
+++ router/router_interface.go
@@ -3,7 +3,11 @@ package router
 import "github.com/casbin/casbin-server/handler"
 
 type Router interface {
+	GET(path string, handler HandlerFunc)
 	POST(path string, handler HandlerFunc)
+	PUT(path string, handler HandlerFunc)
+	DELETE(path string, handler HandlerFunc)
+	OPTIONS(path string, handler HandlerFunc)
 	Serve(addr string) error
 }
 
